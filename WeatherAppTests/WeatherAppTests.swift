34814bff8cce9d1447a2fb9aa9ee697543f614dd
Josip Marković <josip.markovic@factory.hr>
2019-09-10 13:29:47 +0200

    Swift 5 conversion

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Josip Marković on 10/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import Foundation
